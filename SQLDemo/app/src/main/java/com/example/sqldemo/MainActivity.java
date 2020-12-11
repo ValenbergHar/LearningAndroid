1e4a249b88a2cae7ab4c8a82909ccd5e234e55a3
ValenbergHar <maskevich.valeri@gmail.com>
2020-12-11 16:51:50 +0300

    11.12.20

---
+++ SQLDemo/app/src/main/java/com/example/sqldemo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.sqldemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn_view_all, btn_add;
+    private EditText et_name, et_age;
+    private Switch sw_active;
+    private List<Person> person_list;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_view_all = findViewById(R.id.btn_view_all);
+        btn_add = findViewById(R.id.btn_add);
+        et_name = findViewById(R.id.et_name);
+        et_age = findViewById(R.id.et_age);
+
+        recyclerView = findViewById(R.id.rv_customer_list);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter=new RecyclerViewAdapter(person_list, this);
+
+    }
+}
---
+++ SQLDemo/app/src/main/java/com/example/sqldemo/RecyclerViewAdapter.java
@@ -0,0 +1,42 @@
+package com.example.sqldemo;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    public RecyclerViewAdapter(List<Person> person_list, MainActivity mainActivity) {
+
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
