d728ed927d86f2b0d477a7cf16f11c82a99cff4c
ValenbergHar <maskevich.valeri@gmail.com>
2020-12-17 17:22:43 +0300

    17.12.20

--- ArchitectureExample/app/src/main/java/com/example/architectureexample/AddNoteActivity.java
+++ ArchitectureExample/app/src/main/java/com/example/architectureexample/AddNoteActivity.java
@@ -3,6 +3,7 @@ package com.example.architectureexample;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,6 +13,13 @@ import android.widget.NumberPicker;
 import android.widget.Toast;
 
 public class AddNoteActivity extends AppCompatActivity {
+    public static final String EXTRA_TITLE =
+            "com.example.architectureexample.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION =
+            "com.example.architectureexample.EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY =
+            "com.example.architectureexample.EXTRA_PRIORITY";
+
     private EditText editTextTitle;
     private EditText editTextDescription;
     private NumberPicker numberPickerPriority;
@@ -26,7 +34,7 @@ public class AddNoteActivity extends AppCompatActivity {
         numberPickerPriority = findViewById(R.id.number_picker_priority);
 
         numberPickerPriority.setMinValue(1);
-        numberPickerPriority.setValue(10);
+        numberPickerPriority.setMaxValue(10);
 
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_24);
         setTitle("Add Note");
@@ -56,9 +64,14 @@ public class AddNoteActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         String description = editTextDescription.getText().toString();
         int priority = numberPickerPriority.getValue();
-        if(title.trim().isEmpty() || description.trim().isEmpty() ){
+        if (title.trim().isEmpty() || description.trim().isEmpty()) {
             Toast.makeText(this, "Please, insert a title and description", Toast.LENGTH_SHORT).show();
             return;
         }
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, title);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+        data.putExtra(EXTRA_PRIORITY, priority);
+        setResult(RESULT_OK, data);
     }
 }
