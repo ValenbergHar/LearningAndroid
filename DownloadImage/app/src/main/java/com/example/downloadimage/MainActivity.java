d279c318c1ad5524cba1601ab9701f8499fb74cb
ValenbergHar <maskevich.valeri@gmail.com>
2021-01-07 00:33:37 +0300

    07.01.21

---
+++ DownloadImage/app/src/main/java/com/example/downloadimage/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.downloadimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Button button;
+    private EditText editText;
+    private String url = "https://upload.wikimedia.org/wikipedia/commons/9/95/Atrad_Pagonya.png";
+//    private String url ="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.buttonDwnld);
+        editText = findViewById(R.id.editTextTextPersonName);
+//        url = editText.getText().toString();
+        imageView = findViewById(R.id.imageView);
+
+        Log.d("url", url);
+    }
+
+    public void onClickDownloadImage(View view) {
+        DownloadImageTask task = new DownloadImageTask();
+        Bitmap bitmap = null;
+        try {
+            bitmap = task.execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(bitmap);
+    }
+
+    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+    }
+}
