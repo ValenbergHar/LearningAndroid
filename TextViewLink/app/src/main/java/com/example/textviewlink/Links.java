6fd4f5cd951bb93abfe85fe5059eb8f7d1b04e15
ValenbergHar <maskevich.valeri@gmail.com>
2021-02-16 22:13:33 +0300

    16.02.21

---
+++ TextViewLink/app/src/main/java/com/example/textviewlink/Links.java
@@ -0,0 +1,47 @@
+package com.example.textviewlink;
+
+public class Links {
+    private String name;
+    private String link;
+    private int start;
+    private int end;
+
+    public Links(String name, String link, int start, int end) {
+        this.name = name;
+        this.link = link;
+        this.start = start;
+        this.end = end;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+}
--- TextViewLink/app/src/main/java/com/example/textviewlink/MainActivity.java
+++ TextViewLink/app/src/main/java/com/example/textviewlink/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.textviewlink;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.text.HtmlCompat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,13 +14,23 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.text.util.Linkify;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
 
-    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +38,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.text_view);
-        String string = "I want THIS and THIS to be clickable";
+        String string = readRaw(this, R.raw.text);
+        textView.setLinksClickable(true);
+
+//        Linkify.addLinks(textView, Linkify.WEB_URLS);
+
+//        Log.i("qqq", String.valueOf(string.contains("<uline>")));
+//        Log.i("qqq", String.valueOf(string.indexOf("<uline>")));
+        Log.i("qqq", String.valueOf(linkPlusText(string)));
+//        Linkify.addLinks(textView, Linkify.ALL);
+
+
+        Spanned spanned = HtmlCompat.fromHtml(string, HtmlCompat.FROM_HTML_MODE_COMPACT);
+        textView.setText(spanned);
         SpannableString ss = new SpannableString(string);
 
         ClickableSpan clickableSpan1 = new ClickableSpan() {
@@ -43,14 +67,104 @@ public class MainActivity extends AppCompatActivity {
         ClickableSpan clickableSpan2 = new ClickableSpan() {
             @Override
             public void onClick(@NonNull View view) {
+                Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("https://www.youtube.com/watch?v=Kjp6J0blNWQ&t=166s&ab_channel=%D0%91%D0%95%D0%9B%D0%A1%D0%90%D0%A2NEWS"));
+//                        Uri.parse("https://www.google.com/maps/dir/48.8276261,2.3350114/48.8476794,2.340595/48.8550395,2.300022/48.8417122,2.3028844"));
+                startActivity(intent);
+
                 Toast.makeText(MainActivity.this, "Two", Toast.LENGTH_SHORT).show();
             }
         };
-        ss.setSpan(clickableSpan1,7, 11, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss.setSpan(clickableSpan2,16, 20, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(clickableSpan1, 7, 11, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(clickableSpan2, 16, 20, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-        textView.setText(ss);
-        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        //textView.setText(ss);
+        // textView.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
+
+
+    public static String readRaw(Context ctx, int resId) {
+        InputStream inputStream = ctx.getResources().openRawResource(resId);
+        BufferedReader buffreader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        StringBuilder text = new StringBuilder();
+
+        try {
+            while ((line = buffreader.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return text.toString();
+    }
+
+    private static String kingsList(String allKings) {
+        List<String> kings = new ArrayList<>();
+        String string = null;
+        String start = "<text>";
+        String finish = "</text>";
+        Pattern pattern = Pattern.compile(start + "\\s*(.*?)\\s*" + finish, Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(allKings);
+        while (matcher.find()) {
+            string = matcher.group(1);
+        }
+        return string;
+    }
+
+    private static char linkPlusText(String c) {
+        String start = "<a href=\"";
+        String finish = "</a>";
+        String middle = "\">";
+        String linkPlusText = null;
+        int startI = 0;
+        int endI = 0;
+
+
+        startI = c.indexOf(middle) ;
+        endI = c.indexOf(finish) ;
+        char ch1 = c.charAt(startI);
+        char ch2 = c.charAt(endI);
+        String[] lpt;
+        String link = null;
+        String name = null;
+
+        //        String text = "I love you so much";
+//        String wordToFind = "love";
+//        Pattern word = Pattern.compile(wordToFind);
+//        Matcher match = word.matcher(text);
+//
+//        while (match.find()) {
+//            System.out.println("Found love at index "+ match.start() +" - "+ (match.end()-1));
+//        }
+
+        List<Integer> integers = new ArrayList<>();
+        Pattern pattern = Pattern.compile(middle, Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(c);
+        while (matcher.find()){
+         // integers.add(matcher.end());
+        }
+
+        Pattern pattern1 = Pattern.compile(finish, Pattern.DOTALL);
+        Matcher matcher1 = pattern1.matcher(c);
+        while (matcher1.find()){
+            integers.add(matcher1.start()-1);
+        }
+
+
+
+       char ch = c.charAt(105);
+
+        Pattern pattern2 = Pattern.compile(start + "\\s*(.*?)\\s*" + finish, Pattern.DOTALL);
+        Matcher matcher2 = pattern2.matcher(c);
+//        while (matcher2.find()) {
+//            linkPlusText = matcher1.group(0);
+//        }
+//        lpt = linkPlusText.split(middle);
+        return ch;
+    }
+
+
 }
