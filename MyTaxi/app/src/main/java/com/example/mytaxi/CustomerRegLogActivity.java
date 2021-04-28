ac459faa02526c2a5d7fa23e9d2fce3f9e9f6288
ValenbergHar <maskevich.valeri@gmail.com>
2021-04-28 22:33:54 +0300

    28.04.21

--- MyTaxi/app/src/main/java/com/example/mytaxi/CustomerRegLogActivity.java
+++ MyTaxi/app/src/main/java/com/example/mytaxi/CustomerRegLogActivity.java
@@ -1,14 +1,88 @@
 package com.example.mytaxi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import static com.example.mytaxi.R.string.toast_error;
 
 public class CustomerRegLogActivity extends AppCompatActivity {
+    private TextView customer_status, customer_sign_up;
+    private Button customer_btn_login, customer_btn_register;
+    private TextInputEditText customer_email, customer_password;
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_reg_log);
+
+        mAuth = FirebaseAuth.getInstance();
+        loadingBar = new ProgressDialog(this);
+
+        customer_status=findViewById(R.id.customer_status);
+        customer_sign_up=findViewById(R.id.customer_sign_up);
+        customer_btn_login=findViewById(R.id.customer_btn_login);
+        customer_btn_register=findViewById(R.id.customer_btn_register);
+        customer_email=findViewById(R.id.customer_email);
+        customer_password=findViewById(R.id.customer_password);
+
+        customer_btn_register.setVisibility(View.INVISIBLE);
+        customer_btn_register.setEnabled(false);
+
+
+        customer_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customer_btn_register.setVisibility(View.VISIBLE);
+                customer_btn_login.setVisibility(View.INVISIBLE);
+                customer_sign_up.setVisibility(View.INVISIBLE);
+                customer_btn_register.setEnabled(true);
+                customer_status.setText(R.string.set_text_customer);
+            }
+        });
+        customer_btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = customer_email.getText().toString();
+                String password = customer_password.getText().toString();
+                registerCustomer(mail, password);
+            }
+        });
+    }
+
+    private void registerCustomer(String mail, String password) {
+        loadingBar.setTitle(getString(R.string.bar_register_customer));
+        loadingBar.setMessage(getString(R.string.bar_wait));
+        loadingBar.show();
+        mAuth.createUserWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(CustomerRegLogActivity.this,
+                            R.string.toast_ok,
+                            Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                } else {
+                    Toast.makeText(CustomerRegLogActivity.this,
+                            toast_error,
+                            Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+            }
+        });
     }
 }
--- MyTaxi/app/src/main/java/com/example/mytaxi/DriverRegLoginActivity.java
+++ MyTaxi/app/src/main/java/com/example/mytaxi/DriverRegLoginActivity.java
@@ -1,24 +1,37 @@
 package com.example.mytaxi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import static com.example.mytaxi.R.string.toast_error;
 
 public class DriverRegLoginActivity extends AppCompatActivity {
     private TextView driver_status, driver_sign_up;
     private Button driver_btn_login, driver_btn_register;
     private TextInputEditText driver_email, driver_password;
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_reg_login);
+        mAuth = FirebaseAuth.getInstance();
+        loadingBar = new ProgressDialog(this);
+
 
         driver_status = findViewById(R.id.driver_status);
         driver_sign_up = findViewById(R.id.driver_sign_up);
@@ -38,6 +51,38 @@ public class DriverRegLoginActivity extends AppCompatActivity {
                 driver_sign_up.setVisibility(View.INVISIBLE);
                 driver_btn_register.setEnabled(true);
                 driver_status.setText("Рэгістрацыя для кіроўцаў");
+            }
+        });
+
+        driver_btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = driver_email.getText().toString();
+                String password = driver_password.getText().toString();
+                registerDriver(mail, password);
+            }
+        });
+
+    }
+
+    private void registerDriver(String mail, String password) {
+        loadingBar.setTitle(getString(R.string.bar_register_driver));
+        loadingBar.setMessage(getString(R.string.bar_wait));
+        loadingBar.show();
+        mAuth.createUserWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(DriverRegLoginActivity.this,
+                            R.string.toast_ok,
+                            Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                } else {
+                    Toast.makeText(DriverRegLoginActivity.this,
+                            toast_error,
+                            Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
             }
         });
 
